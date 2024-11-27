8abc78811adf78678f01f70522170fd196b49115
CANMERTINYO <38213551+c4nzin@users.noreply.github.com>
2024-11-27 10:51:38 +0300

    refactor: fix logic

--- src/decorators/rate-limit.decorator.ts
+++ src/decorators/rate-limit.decorator.ts
@@ -1,7 +1,10 @@
-import { SetMetadata } from "@nestjs/common";
+import { CustomDecorator, SetMetadata } from "@nestjs/common";
 
 export const RATE_LIMITER_OPTIONS = "rateLimiterOptions";
 
-export const RateLimit = (options: { ms: number; maxRequest: number }) => {
-  SetMetadata(RATE_LIMITER_OPTIONS, options);
+export const RateLimit = (options: {
+  ms: number;
+  maxRequest: number;
+}): CustomDecorator => {
+  return SetMetadata(RATE_LIMITER_OPTIONS, options);
 };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./decorators/rate-limit.decorator";
+export * from "./guards/rate-limit.guard";
+export * from "./interfaces/store.interface";
+export * from "./middlewares/rate-limit.middleware";
+export * from "./modules/rate-limit.module";
